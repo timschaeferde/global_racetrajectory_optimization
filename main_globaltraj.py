9a202bcaf23edb46ed2a96bb2d2b0f4a6601131a
Tim Schaefer <tim.schaefer.de@gmail.com>
2021-06-02 18:26:08 +0200

    new ax_max

--- main_globaltraj.py
+++ main_globaltraj.py
@@ -57,7 +57,7 @@ imp_opts = {"flip_imp_track": False,                # flip imported track to rev
 # 'mincurv'             minimum curvature optimization without iterative call
 # 'mincurv_iqp'         minimum curvature optimization with iterative call
 # 'mintime'             time-optimal trajectory optimization
-opt_type = 'mincurv_iqp'
+opt_type = 'mintime'
 
 # set mintime specific options (mintime only) --------------------------------------------------------------------------
 # tpadata:                      set individual friction map data file if desired (e.g. for varmue maps), else set None,
